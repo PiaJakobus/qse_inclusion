051a75f891f5722c22d3c5abef4a07e5e5a4b945
PiaJak <pia.jakobus@gmx.de>
2022-05-29 16:30:22 +1000

    removed binary search

--- read_qse.f95
+++ read_qse.f95
@@ -3,6 +3,8 @@ use hdf5
 use hdf5_utils
 use iso_fortran_env, only: dp => real64
 use linspace_mod
+!use variableKind
+!use m_searching, only: intervalSearch
 
 implicit none
 
@@ -21,9 +23,7 @@ subroutine data_qse(y,path)
   integer(HID_T)                   :: file_id,group_id,root_id, d_id
   character(len=60)                :: dataname, rootname, filename
 
-  filename = "QSE_table.jld"
-  !print*, filename
-  !stop
+  filename = path!"QSE_table.jld"
   rootname = "/"
   dataname = "/"//"data"
   call open_hdf5file(filename,file_id,errorflag)
@@ -100,17 +100,7 @@ subroutine qse(rho,temp,ye,x,x_cl,enbyrst)
       real(kind=8), intent(inout) :: x(5371)
       real(kind=8)                :: y(75,1,1,75,5371)
       character(len=60) :: path
-      !real(kind=8)                :: yrange(75), rrange(64),trange(75),srange(75)
-      !integer :: j
-      !call range_params(yrange, "/yrange")
-      !call range_params(srange, "/yrange")
-      !call data_qse(y,path)
-      !stop
-      !print*, yrange
-      !trange = linspace(3e9_dp,5e9_dp,75) !trange
-      !rrange =  linspace(1e8_dp,5e9_dp,64) !rrange
-      call interpolate4D(1.e8_dp,3.e9_dp,0.49_dp,-3._dp,2,x(1))
-      !print*, x(1)
+      call interpolate4D(1.52e8_dp,4.468e9_dp,0.4974_dp,-3._dp,2,x)
 end subroutine qse
 
 subroutine interpolate4D(rho,tem,ye,cl,index_part,x_inter)
@@ -122,69 +112,113 @@ subroutine interpolate4D(rho,tem,ye,cl,index_part,x_inter)
       implicit none
       real(kind=8), intent(in)  :: rho,tem,ye,cl
       integer, intent(in)       ::  index_part
-      real(kind=8), intent(out) :: x_inter
+      real(kind=8), intent(out) :: x_inter(5371)
       real(kind=8)              :: yrange(75), rrange(64),trange(75),srange(75)
-      real(kind=8)              :: fl(75,1,1,75,5371)
+      real(kind=8)              :: fl1(75,1,1,75,5371)
+      real(kind=8)              :: fl2(75,1,1,75,5371)
+      real(kind=8)              :: fl3(75,1,1,75,5371)
+      real(kind=8)              :: fl4(75,1,1,75,5371)
       ! real(kind=8)              :: f11(75,1,1,75,5371)
       ! real(kind=8)              :: f22(75,1,1,75,5371)
       ! real(kind=8)              :: f12(75,1,1,75,5371)
       ! real(kind=8)              :: f21(75,1,1,75,5371)
-      integer                   :: i_r,i_t,i_ye,i_cl
+      integer                   :: i_r,r,i_t,i_ye,i_cl, j
       real(kind=8)              :: x1,x2,x3,x4,&
                                    x5,x6,x7,x8,&
                                    x9,x10,x11,x12,&
                                    x13,x14,x15,x16
       real(kind=8)              :: w10,w20,w30,w40,&
                                    w11,w21,w31,w41
-      character(len=60)         :: path1,path2,path3,path4
+      character(len=60)         :: path1,path2,path3,path4,tmp1,tmp2
 
       rrange = linspace(1e8_dp,1e9_dp,64)
+      !print*, rrange
+      !print*,"----------------------------"
       trange = linspace(5e9_dp,3e9_dp,75)
       yrange = linspace(0.5_dp,0.47_dp,75)
-      print*, yrange
-      srange = linspace(-1.5._dp,-4._dp,75)
-      path1 = ""
-      path2 = ""
-      path3 = ""
-      path4 = ""
+      srange = linspace(-1.5_dp,-4._dp,75)
+
       !call range_params(yrange, "/yrange")
       !call range_params(srange, "/srange")
-      !print*,"test"
-      call binary_search(rrange,rho,i_r)
-      call binary_search(trange,tem,i_t)
-      call binary_search(yrange,ye,i_t)
-      call binary_search(srange,cl,i_t)
-      !print*,"test"
-      ! now convert i_r,i_t into dir-names !!!
-      call data_qse(fl,path1)
-      print*,i_ye,i_cl
-      !i_cl = 1
-      !print*,fl(:,1,1,:,1)
-      x1 = fl(i_ye,1,1,i_cl,1)      ! 0000
-      x5 = fl(i_ye+1,1,1,i_cl,1)    ! 0010
-      x6 = fl(i_ye,1,1,i_cl+1,1)    ! 0001
-      x8 = fl(i_ye+1,1,1,i_cl+1,1)  ! 0011
-      call data_qse(fl,path2)
-      print*,"test"
-      x4  = fl(i_ye,1,1,i_cl,1)     ! 0100
-      x10 = fl(i_ye,1,1,i_cl+1,1)   ! 0101
-      x11 = fl(i_ye+1,1,1,i_cl,1)   ! 0110
-      x16 = fl(i_ye+1,1,1,i_cl+1,1) ! 0111
-      call data_qse(fl,path3)
-      x3  = fl(i_ye,1,1,i_cl,1)     ! 1000
-      x9  = fl(i_ye+1,1,1,i_cl,1)   ! 1010
-      x12 = fl(i_ye,1,1,i_cl+1,1)   ! 1001
-      x15 = fl(i_ye+1,1,1,i_cl+1,1) ! 1011
-      call data_qse(fl,path4)
-      x2  = fl(i_ye+1,1,1,i_cl+1,1)  ! 1111
-      x7  = fl(i_ye,1,1,i_cl,1)      ! 1100
-      x13 = fl(i_ye+1,1,1,i_cl,1)    ! 1110
-      x14 = fl(i_ye,1,1,i_cl+1,1)    ! 1101
+
+      i_r = MINLOC(rrange, dim=1,mask=(rho < rrange)) ! i_r is left grid point
+      !print*, "rrange====",rrange(i_r-1), rrange(i_r),rho
+      i_t = MINLOC(trange, dim=1,mask=(tem < trange)) ! reverse because descending list
+      !print*, "trange====",trange(i_t+1), trange(i_t),tem
+      i_ye = MINLOC(yrange, dim=1,mask=(ye < yrange)) ! reverse because descending list
+      !print*, "yrange====",yrange(i_t+1), yrange(i_ye),ye
+      i_cl = MINLOC(abs(srange), dim=1,mask=(abs(cl) < abs(srange))) ! i_s is left grid point
+      !print*, "srange====",srange(i_cl-1), srange(i_cl),cl, i_cl
+
+
+
+      i_r = 1
+      i_t = 1
+      i_cl = 10
+      i_ye = 10
+      write(tmp1,'(a,i0,a)') 'r',i_r-1
+      write(tmp2,'(a,i0,a)') '/t',i_t
+      path1 = "qse_table/"//trim(tmp1) // trim(tmp2) // "/QSE_table.jld"
+      write(tmp1,'(a,i0,a)') 'r',i_r-1
+      write(tmp2,'(a,i0,a)') '/t',i_t+1
+      path2 = "qse_table/"//trim(tmp1) // trim(tmp2) // "/QSE_table.jld"
+      write(tmp1,'(a,i0,a)') 'r',i_r
+      write(tmp2,'(a,i0,a)') '/t',i_t
+      path3 = "qse_table/"//trim(tmp1) // trim(tmp2) // "/QSE_table.jld"
+      write(tmp1,'(a,i0,a)') 'r',i_r
+      write(tmp2,'(a,i0,a)') '/t',i_t+1
+      path4 = "qse_table/"//trim(tmp1) // trim(tmp2) // "/QSE_table.jld"
+
+      call data_qse(fl1,path1)
+      call data_qse(fl2,path2)
+      call data_qse(fl3,path3)
+      call data_qse(fl4,path4)
+
+
+
+
+      w10 = (rrange(i_r+1) - rho) / (rrange(i_r+1) - rrange(i_r))
+      w11 = 1. - w10
+      w20 = (trange(i_t+1) - tem) / (trange(i_t+1) - trange(i_t))
+      w21 = 1. - w20
+      w30 = (yrange(i_ye+1) - ye) / (yrange(i_ye+1) - yrange(i_ye))
+      w31 = 1. - w30
+      w40 = (srange(i_ye+1) - cl) / (srange(i_ye+1) - srange(i_ye))
+      w41 = 1. - w40
+
+print*, w10,w20,w30,w40,w11,w21,w31,w41
+
+
+
+      do j = 1, size(x_inter)
+      x1 = fl1(i_ye,1,1,i_cl,j)      ! 0000
+      x5 = fl1(i_ye+1,1,1,i_cl,j)    ! 0010
+      x6 = fl1(i_ye,1,1,i_cl+1,j)    ! 0001
+      x8 = fl1(i_ye+1,1,1,i_cl+1,j)  ! 0011
+
+      x4  = fl2(i_ye,1,1,i_cl,j)     ! 0100
+      x10 = fl2(i_ye,1,1,i_cl+1,j)   ! 0101
+      x11 = fl2(i_ye+1,1,1,i_cl,j)   ! 0110
+      x16 = fl2(i_ye+1,1,1,i_cl+1,j) ! 0111
+
+      x3  = fl3(i_ye,1,1,i_cl,j)     ! 1000
+      x9  = fl3(i_ye+1,1,1,i_cl,j)   ! 1010
+      x12 = fl3(i_ye,1,1,i_cl+1,j)   ! 1001
+      x15 = fl3(i_ye+1,1,1,i_cl+1,j) ! 1011
+
+      x2  = fl4(i_ye+1,1,1,i_cl+1,j)  ! 1111
+      x7  = fl4(i_ye,1,1,i_cl,j)      ! 1100
+      x13 = fl4(i_ye+1,1,1,i_cl,j)    ! 1110
+      x14 = fl4(i_ye,1,1,i_cl+1,j)    ! 1101
 
 ! =================================================
-! TODO: i_cl, i_cl not working because reverse:
-! TODO: path directories einpflegen
-! TODO: Loop fuer alle elements adden
+! TODO: i_cl, i_cl not working because reverse: DONE!
+! TODO: path directories einpflegen: DONE!
+! TODO: Loop fuer alle elements adden : DONE!
+! TODO: fix binary search -> DONE
+! TODO: run on harddrive
+! TODO: write tests
+! TODO: one_zone.f95
 ! =================================================
 ! testing_QSE(ARGS,
 ! collect(LinRange(0.5,0.47,75)),
@@ -211,16 +245,10 @@ subroutine interpolate4D(rho,tem,ye,cl,index_part,x_inter)
       ! x15 = f21(i_ye+1,i_cl+1) ! 1011
       ! x16 = f12(i_ye+1,i_cl+1) ! 0111
       ! 12 weights:
-      w10 = (rrange(i_r+1) - rho) / (rrange(i_r+1) - rrange(i_r))
-      w11 = 1. - w10
-      w20 = (trange(i_t+1) - tem) / (trange(i_t+1) - trange(i_t))
-      w21 = 1. - w20
-      w30 = (yrange(i_ye+1) - ye) / (yrange(i_ye+1) - yrange(i_ye))
-      w31 = 1. - w30
-      w40 = (srange(i_ye+1) - cl) / (srange(i_ye+1) - srange(i_ye))
-      w41 = 1. - w40
 
-      x_inter = x1 * (w10 * w20 * w30 * w40) + &
+
+
+      x_inter(j) = x1 * (w10 * w20 * w30 * w40) + &
                 x2 * (w11 * w21 * w31 * w41) + &
                 x3 * (w11 * w20 * w30 * w40) + &
                 x4 * (w10 * w21 * w30 * w40) + &
@@ -236,41 +264,9 @@ subroutine interpolate4D(rho,tem,ye,cl,index_part,x_inter)
                 x14 * (w11 * w21 * w30 * w41) + &
                 x15 * (w11 * w20 * w31 * w41) + &
                 x16 * (w10 * w21 * w31 * w41)
-        print*, x_inter
+        end do
       end subroutine interpolate4D
 
-subroutine binary_search(x,x1,i0)
- implicit none
- real(kind=8), intent(in) :: x(:)
- real(kind=8), intent(in) :: x1
- integer                  :: i,i1
- integer, intent(inout)   :: i0
- i0 = 1
- i1 = size(x)
- ! test if x1 is outside of interval [x(1), x(end)]
- if (x1 < x(i0)) then
-   i = i0
-   print*, "Outside range in binary search"
-   return
- end if
- if (x1 > x(i1)) then
-   i = i1
-   print*, "Outside range in binary search"
-   return
- end if
 
- ! binary search
- do while (i1 > (i0 + 1))
-   i = (i1 + i0) / 2
-   if (x(i) < x1) then
-     i0 = i
-   else if (x(i) > x1) then
-     i1 = i
-   else
-     return
-   end if
- end do
- !i = merge(i0, i1, ((2 * x1) < (x(i0) + x(i1))))
-end subroutine binary_search
 
 end module read_qse
